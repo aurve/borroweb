f50f742e528cdc8ef5eee82fb1a5d6b9b58c7070
aurve <clark.aurve@gmail.com>
2023-07-04 20:05:56 +0800

    test

--- navigation/screens/earnings.js
+++ navigation/screens/earnings.js
@@ -10,55 +10,7 @@ export default function Earnings({navigation}) {
       <ScrollView contentContainerStyle={{flexGrow: 1, backgroundColor: '#fff'}} showsVerticalScrollIndicator={false} indicatorStyle="white">
       <View style={styles.content}>
       <Text style={styles.text}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non
-          tortor a sem consectetur venenatis. Nulla iaculis, odio nec pretium
-          lobortis, sapien urna commodo odio, eget volutpat ligula risus sed
-          libero. Proin ultricies blandit nisl, non rutrum erat rutrum vitae.
-          Sed tempus velit vitae ex scelerisque, sit amet mattis nulla
-          vestibulum. Sed at metus sed elit sollicitudin iaculis. Suspendisse
-          vel orci finibus, sollicitudin velit et, pellentesque sapien.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non
-          tortor a sem consectetur venenatis. Nulla iaculis, odio nec pretium
-          lobortis, sapien urna commodo odio, eget volutpat ligula risus sed
-          libero. Proin ultricies blandit nisl, non rutrum erat rutrum vitae.
-          Sed tempus velit vitae ex scelerisque, sit amet mattis nulla
-          vestibulum. Sed at metus sed elit sollicitudin iaculis. Suspendisse
-          vel orci finibus, sollicitudin velit et, pellentesque sapien.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non
-          tortor a sem consectetur venenatis. Nulla iaculis, odio nec pretium
-          lobortis, sapien urna commodo odio, eget volutpat ligula risus sed
-          libero. Proin ultricies blandit nisl, non rutrum erat rutrum vitae.
-          Sed tempus velit vitae ex scelerisque, sit amet mattis nulla
-          vestibulum. Sed at metus sed elit sollicitudin iaculis. Suspendisse
-          vel orci finibus, sollicitudin velit et, pellentesque sapien.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non
-          tortor a sem consectetur venenatis. Nulla iaculis, odio nec pretium
-          lobortis, sapien urna commodo odio, eget volutpat ligula risus sed
-          libero. Proin ultricies blandit nisl, non rutrum erat rutrum vitae.
-          Sed tempus velit vitae ex scelerisque, sit amet mattis nulla
-          vestibulum. Sed at metus sed elit sollicitudin iaculis. Suspendisse
-          vel orci finibus, sollicitudin velit et, pellentesque sapien.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non
-          tortor a sem consectetur venenatis. Nulla iaculis, odio nec pretium
-          lobortis, sapien urna commodo odio, eget volutpat ligula risus sed
-          libero. Proin ultricies blandit nisl, non rutrum erat rutrum vitae.
-          Sed tempus velit vitae ex scelerisque, sit amet mattis nulla
-          vestibulum. Sed at metus sed elit sollicitudin iaculis. Suspendisse
-          vel orci finibus, sollicitudin velit et, pellentesque sapien.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non
-          tortor a sem consectetur venenatis. Nulla iaculis, odio nec pretium
-          lobortis, sapien urna commodo odio, eget volutpat ligula risus sed
-          libero. Proin ultricies blandit nisl, non rutrum erat rutrum vitae.
-          Sed tempus velit vitae ex scelerisque, sit amet mattis nulla
-          vestibulum. Sed at metus sed elit sollicitudin iaculis. Suspendisse
-          vel orci finibus, sollicitudin velit et, pellentesque sapien.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non
-          tortor a sem consectetur venenatis. Nulla iaculis, odio nec pretium
-          lobortis, sapien urna commodo odio, eget volutpat ligula risus sed
-          libero. Proin ultricies blandit nisl, non rutrum erat rutrum vitae.
-          Sed tempus velit vitae ex scelerisque, sit amet mattis nulla
-          vestibulum. Sed at metus sed elit sollicitudin iaculis. Suspendisse
-          vel orci finibus, sollicitudin velit et, pellentesque sapien.
+          
         </Text>
       </View>
       </ScrollView>
@@ -70,7 +22,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    margin: 24,
   },
   content: {
     flex: 1,
